cb054aea09f6fcaf17a46c348bdd27c2200855ef
Shobhit Bhardwaj <137488424+ShobhitBhardwaj763@users.noreply.github.com>
2025-07-07 18:19:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+import firebase_admin 
+from firebase_admin import credentials, firestore
+from firebase_admin import auth
+
+
+
+st.set_page_config(page_title="AI Doubt Solver", page_icon="🤖", layout="wide")
+
+with st.sidebar:
+    st.title("AI Doubt Solver")
+    st.title("Sessions")
+    st.button("➕ New Session", on_click=lambda: st.session_state.update(messages=[]))
+    st.markdown("Powered by **Gemini API** + **Firebase**")
+    st.markdown("---")
+    st.markdown("💬 Ask any academic doubt, get instant help, and review past Q&A.")
+    st.caption("Made by Shobhit 🚀")
+
+
+api_key = st.secrets["GEMINI_API_KEY"]
+    
+# Intialize session state variable
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+st.title("AI Doubt Solver")
+st.write("Ask me any question, and I’ll help you understand it better!")
+
+
+# Load API key from .env file
+genai.configure(api_key=api_key)
+
+# Ask user for a qustion
+# User input box
+if question := st.chat_input("Ask your doubt here..."):
+    # Display user message
+    st.chat_message("user").markdown(question)
+    st.session_state.messages.append({"role": "user", "content": question})
+
+#Generate AI response
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            model = genai.GenerativeModel("gemini-1.5-flash")
+            prompt = f"Answer in a simple and clear way:\n\n{question}"
+            response = model.generate_content(prompt)
+            st.markdown(response.text)
+            st.session_state.messages.append({"role": "assistant", "content": response.text})
+
+
+
+
+
+ #save to firebase
+        db = firestore.client()  
+        db.collection("doubts").add({
+            "question": question,
+            "answer": response.text,
+            "user" : "anonymous",
+        })       
+
+
+
+    
+    
+
+#Firebase Setup
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
